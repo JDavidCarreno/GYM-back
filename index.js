1e3b86d6934084687f352ce08aa812805211f0bf
CamiloCabezas <116015687+CamiloCabezas@users.noreply.github.com>
2023-08-24 21:50:11 -0500

    Fixed create rout

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-const server = require('./src/app');
-const { conn } = require('./src/db');
 const { PORT } = process.env;
+const express = require('express');
+const morgan = require('morgan');
+const router = require('./src/routes/index');
+
+const app = express();
 
 
-conn.sync({force: false}).then(() => {
-    server.listen(PORT, () => {
-        console.log(`Listening on port: ${PORT}`);
-    })
+
+app.use(express.json())
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+       'Access-Control-Allow-Headers',
+       'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header(
+       'Access-Control-Allow-Methods',
+       'GET, POST, OPTIONS, PUT, DELETE'
+    );
+    next();
+ });
+
+app.use('/', router);
+
+const server = app.listen(3001, () => {
+    console.log(`Listening on port: 3001`)
 })
 
+// conn.sync({force: false}).then(() => {
+//     server.listen(PORT, () => {
+//         console.log(`Listening on port: ${PORT}`);
+//     })
+// })
 
+/*const server = app.listen(3000, () =>
+  console.log(`
+🚀 Server ready at: http://localhost:3000
+⭐️ See sample requests: http://pris.ly/e/js/rest-express#3-using-the-rest-api`),
+) */
